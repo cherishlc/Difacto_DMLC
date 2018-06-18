0d4cdc5dd347f00aff8f20028cbe2717b422254a
cherishlc <lcreg163@163.com>
2018-06-18 16:36:15 +0800

    change run_local.sh

--- makeall.sh
+++ makeall.sh
@@ -1,3 +1,19 @@
+BIN_PATH=`readlink -f $0`
+BIN_PATH=`dirname $BIN_PATH`
+
+
+# suppose glog is in ../glog
+export GLOG_HOME=`realpath $BIN_PATH/../glog`  
+echo $GLOG_HOME
+
+# g++ header file path 
+export CPLUS_INCLUDE_PATH=$CPLUS_INCLUDE_PATH:$GLOG_HOME/src 
+export EXTRA_INCLUDES=$GLOG_HOME/src  
+# g++ libfile path  
+export LIBRARY_PATH=$LIBRARY_PATH:$GLOG_HOME/.libs  
+# .so file path  
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$GLOG_HOME/.libs 
+ 
 export MAKEDMLC="make USE_HDFS=1 USE_GLOG=1 -j8"  
 cd dmlc-core; $MAKEDMLC; cd -  
 cd ps-lite; $MAKEDMLC; cd -  
